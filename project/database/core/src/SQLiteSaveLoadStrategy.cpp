e744c1c29326420aa891eaf91bf3feb3ab5953fb
Vitalii Demediuk <demedyuk.v.i@gmail.com>
2022-11-15 18:28:10 +0200

    SQLite. Implemented drop all tables

--- project/database/core/src/SQLiteSaveLoadStrategy.cpp
+++ project/database/core/src/SQLiteSaveLoadStrategy.cpp
@@ -2,12 +2,16 @@
 
 #include <iostream>
 
+// Boost
+#include <boost/format.hpp>
+
 // Local
 #include "StringUtils.h"
 #include "VirtualDatabase.h"
 
 // SQLite
 #include <SQLiteCpp/Database.h>
+#include <SQLiteCpp/Transaction.h>
 
 core::save_load::SQLiteStrategy::SQLiteStrategy(SQLiteInfo sqliteInfo)
     : fSQLiteInfo(std::move(sqliteInfo)) {}
@@ -17,6 +21,8 @@ namespace sql_query
     constexpr const char* GET_ALL_TABLE_NAMES = "SELECT name FROM sqlite_schema "
                                                 "WHERE type = 'table' AND name NOT LIKE 'sqlite_%' "
                                                 "ORDER BY 1;";
+
+    constexpr const wchar_t* DROP_TABLE = L"DROP TABLE %1%";
 }
 
 namespace {
@@ -30,10 +36,17 @@ namespace {
         return res;
     }
 
+    void dropTable(SQLite::Database& sqlDb, std::wstring tableName)
+    {
+        const std::wstring query = (boost::wformat(sql_query::DROP_TABLE) % tableName).str();
+        sqlDb.exec(core::utils::wstringToUtf8(query));
+    }
+
     void dropAllTables(SQLite::Database& sqlDb)
     {
-        for (const auto& name : getTableNames(sqlDb)) {
-            std::wcout << name << std::endl;
+        const auto tableNames = getTableNames(sqlDb);
+        for (auto& tableName : tableNames) {
+            dropTable(sqlDb, tableName);
         }
     }
 }
@@ -42,11 +55,13 @@ void core::save_load::SQLiteStrategy::save(const VirtualDatabase &db) const
 {
     SQLite::Database sqlDb(fSQLiteInfo.filePath, SQLite::OPEN_READWRITE|SQLite::OPEN_CREATE);\
     dropAllTables(sqlDb);
+    SQLite::Transaction transaction(sqlDb);
+    transaction.commit();
+
+
 }
 
 std::unique_ptr<core::VirtualDatabase> core::save_load::SQLiteStrategy::load() const
 {
-    SQLite::Database sqlDb(fSQLiteInfo.filePath, SQLite::OPEN_READWRITE|SQLite::OPEN_CREATE);\
-    dropAllTables(sqlDb);
     return {};
 }
