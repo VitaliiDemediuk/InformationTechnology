f25665bf431e0a79f3c4cf0541fd6d8ba96432a9
Vitalii Demediuk <demedyuk.v.i@gmail.com>
2022-11-19 18:14:52 +0200

    SQLite. Load column info.

--- project/database/core/src/SQLiteSaveLoadStrategy.cpp
+++ project/database/core/src/SQLiteSaveLoadStrategy.cpp
@@ -2,12 +2,18 @@
 
 #include <iostream>
 
+// STD
+#include <regex>
+
 // Boost
 #include <boost/format.hpp>
+#include <boost/lexical_cast.hpp>
+#include <boost/algorithm/string.hpp>
 
 // Local
+#include "Database.h"
+#include "CustomTable.h"
 #include "StringUtils.h"
-#include "VirtualDatabase.h"
 
 // SQLite
 #include <SQLiteCpp/Database.h>
@@ -81,7 +87,7 @@ namespace {
                 const auto limitColumn = static_cast<const core::IntervalIntColumnInfo&>(column);
                 return (boost::wformat(LR"("%1%" %2% CHECK("%1%" BETWEEN %3% AND %4%))")
                         % column.name()
-                        % sql_type::blob
+                        % sql_type::integer
                         % limitColumn.lowerLimit()
                         % limitColumn.upperLimit()).str();
             }
@@ -94,7 +100,7 @@ namespace {
 
     void createTable(SQLite::Database& sqlDb, const core::VirtualTable& table)
     {
-        std::wstring query = L"CREATE TABLE table_name (";
+        std::wstring query = (boost::wformat(L"CREATE TABLE %1% (") % table.name()).str();
         const size_t nColumn = table.columnCount();
         for (size_t i = 0; i < nColumn; ++i) {
             query.append(getCreateColumnLine(table.column(i)));
@@ -130,7 +136,118 @@ void core::save_load::SQLiteStrategy::save(const VirtualDatabase &db) const
     transaction.commit();
 }
 
+namespace
+{
+
+struct SQLiteColumnInfo
+{
+    std::wstring name;
+    std::wstring type;
+};
+
+auto getTableColumnNames(SQLite::Database& sqlDb, const std::wstring& tableName) -> std::vector<SQLiteColumnInfo>
+{
+    const auto columnNamesQuery = (boost::wformat(LR"(PRAGMA table_info("%1%");)") % tableName).str();
+    SQLite::Statement allColumnInfoQuery(sqlDb, core::utils::wstringToUtf8(columnNamesQuery));
+    std::vector<SQLiteColumnInfo> res;
+    while(allColumnInfoQuery.executeStep()) {
+        constexpr static size_t nameIdx = 1;
+        constexpr static size_t typeIdx = 2;
+        res.push_back({.name = core::utils::utf8ToWstring(allColumnInfoQuery.getColumn(nameIdx).getString()),
+                       .type = core::utils::utf8ToWstring(allColumnInfoQuery.getColumn(typeIdx).getString())});
+    }
+    return res;
+}
+
+std::wstring getSqlCreateTableQuery(SQLite::Database& sqlDb, const std::wstring& tableName)
+{
+    const auto queryStr = (boost::wformat(LR"(SELECT sql FROM sqlite_master
+                                                 WHERE tbl_name = "%1%" AND type = "table")") % tableName ).str();
+    SQLite::Statement query(sqlDb, core::utils::wstringToUtf8(queryStr));
+    if (query.executeStep()) {
+        return core::utils::utf8ToWstring(query.getColumn(0).getString());
+    }
+    return {};
+}
+
+auto getColumnInfo(const SQLiteColumnInfo& sqliteColumnInfo, const std::wstring& createTableQuery)
+    -> std::unique_ptr<core::VirtualColumnInfo>
+{
+    auto tableName = sqliteColumnInfo.name;
+
+    const auto createColumnLineWithCheck = [&tableName, &createTableQuery] () -> std::wstring {
+        const std::wstring regexStr = (boost::wformat(LR"("%1%"[^,]*?CHECK.+?(,|\)\)))") % tableName).str();
+        const std::wregex regex{regexStr};
+        std::wsmatch sm;
+        if (std::regex_search(createTableQuery, sm, regex)) {
+            return sm.str();
+        }
+        return {};
+    }();
+
+    std::wcout << createColumnLineWithCheck << std::endl;
+
+    std::unique_ptr<core::VirtualColumnInfo> colInfo;
+    if (sqliteColumnInfo.type == sql_type::integer) {
+        if (createColumnLineWithCheck.empty()) {
+            colInfo = std::make_unique<core::ColumnInfo>(core::DataType::INTEGER, sqliteColumnInfo.name);
+        } else {
+            static const std::wregex numberRegex(LR"([+-]?\b[0-9]+\b)");
+            auto bIt = std::wsregex_iterator(createColumnLineWithCheck.begin(), createColumnLineWithCheck.end(), numberRegex);
+            const auto numbers = std::vector(bIt, std::wsregex_iterator());
+            if (numbers.size() != 2) {
+                throw std::logic_error("Invalid createColumnLineWithCheck. Expected two numbers");
+            }
+
+            const auto lowerLimit = boost::lexical_cast<int64_t>(numbers[0].str());
+            const auto upperLimit = boost::lexical_cast<int64_t>(numbers[1].str());
+            colInfo = std::make_unique<core::IntervalIntColumnInfo>(lowerLimit, upperLimit, sqliteColumnInfo.name);
+        }
+    } else if (sqliteColumnInfo.type == sql_type::real) {
+        colInfo = std::make_unique<core::ColumnInfo>(core::DataType::REAL, sqliteColumnInfo.name);
+    } else if (sqliteColumnInfo.type == sql_type::text) {
+        if (createColumnLineWithCheck.empty()) {
+            colInfo = std::make_unique<core::ColumnInfo>(core::DataType::STRING, sqliteColumnInfo.name);
+        } else {
+            colInfo = std::make_unique<core::ColumnInfo>(core::DataType::CHAR, sqliteColumnInfo.name);
+        }
+    } else if (sqliteColumnInfo.type == sql_type::blob) {
+        colInfo = std::make_unique<core::ColumnInfo>(core::DataType::TEXT_FILE, sqliteColumnInfo.name);
+    }
+    return colInfo;
+}
+
+void getTableFromSQLiteDb(SQLite::Database& sqlDb, core::VirtualTable& table)
+{
+    const auto& tableName = table.name();
+    const auto sqliteColumnsInfo = getTableColumnNames(sqlDb, tableName);
+    const auto createTableQuery = [&sqlDb, &tableName] () {
+        auto query = getSqlCreateTableQuery(sqlDb, tableName);
+        boost::algorithm::trim(query);
+        return query;
+    } () ;
+
+    for (size_t i = 1; i < sqliteColumnsInfo.size(); ++i) {
+        table.createColumn(getColumnInfo(sqliteColumnsInfo[i], createTableQuery));
+    }
+}
+
+void getTablesFromSQLiteDb(SQLite::Database& sqlDb, core::Database& db)
+{
+    const auto tableNames = getTableNames(sqlDb);
+    for (const auto& tableName : tableNames) {
+        auto& table = db.createTable(tableName);
+        getTableFromSQLiteDb(sqlDb, table);
+    }
+}
+
+} // anon namespace
+
 std::unique_ptr<core::VirtualDatabase> core::save_load::SQLiteStrategy::load() const
 {
-    return {};
+    SQLite::Database sqlDb(fSQLiteInfo.filePath, SQLite::OPEN_READWRITE|SQLite::OPEN_CREATE);
+    auto db = std::make_unique<core::Database>(fSQLiteInfo.filePath.stem().wstring(),
+                                               std::make_unique<core::CustomTableFactory>());
+    getTablesFromSQLiteDb(sqlDb, *db);
+    return db;
 }
