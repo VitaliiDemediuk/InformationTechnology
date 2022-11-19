f5b44586a00430f932b3059c5fe7739f49ec0f67
Vitalii Demediuk <demedyuk.v.i@gmail.com>
2022-11-19 13:26:18 +0200

    SQLite. Implemented table creating(need testing).

--- project/database/core/src/SQLiteSaveLoadStrategy.cpp
+++ project/database/core/src/SQLiteSaveLoadStrategy.cpp
@@ -25,6 +25,14 @@ namespace sql_query
     constexpr const wchar_t* DROP_TABLE = L"DROP TABLE %1%";
 }
 
+namespace sql_type
+{
+    constexpr std::wstring_view integer = L"INTEGER";
+    constexpr std::wstring_view real = L"REAL";
+    constexpr std::wstring_view text = L"TEXT";
+    constexpr std::wstring_view blob = L"BLOB";
+}
+
 namespace {
     std::vector<std::wstring> getTableNames(SQLite::Database& sqlDb)
     {
@@ -49,16 +57,76 @@ namespace {
             dropTable(sqlDb, tableName);
         }
     }
-}
+
+    std::wstring getCreateColumnLine(const core::VirtualColumnInfo& column)
+    {
+        using T = core::DataType;
+        switch (column.dataType()) {
+            case T::INTEGER: {
+                return (boost::wformat(LR"("%1%" %2%)") % column.name() % sql_type::integer).str();
+            }
+            case T::REAL: {
+                return (boost::wformat(LR"("%1%" %2%)") % column.name() % sql_type::real).str();
+            }
+            case T::CHAR: {
+                return (boost::wformat(LR"("%1%" %2% CHECK(LENGTH("%1%") <= 1))") % column.name() % sql_type::text).str();
+            }
+            case T::STRING: {
+                return (boost::wformat(LR"("%1%" %2%)") % column.name() % sql_type::text).str();
+            }
+            case T::TEXT_FILE: {
+                return (boost::wformat(LR"("%1%" %2%)") % column.name() % sql_type::blob).str();
+            }
+            case T::INTERVAL_INTEGER: {
+                const auto limitColumn = static_cast<const core::IntervalIntColumnInfo&>(column);
+                return (boost::wformat(LR"("%1%" %2% CHECK("%1%" BETWEEN %3% AND %4%))")
+                        % column.name()
+                        % sql_type::blob
+                        % limitColumn.lowerLimit()
+                        % limitColumn.upperLimit()).str();
+            }
+            case T::NN: {
+                throw std::logic_error("SQLite. Invalid data type during database creating");
+            }
+        }
+        throw std::logic_error("SQLite. Not implemented.");
+    }
+
+    void createTable(SQLite::Database& sqlDb, const core::VirtualTable& table)
+    {
+        std::wstring query = L"CREATE TABLE table_name (";
+        const size_t nColumn = table.columnCount();
+        for (size_t i = 0; i < nColumn; ++i) {
+            query.append(getCreateColumnLine(table.column(i)));
+            if (i < nColumn - 1) {
+                query.append(L", ");
+            }
+        }
+        query.append(L");");
+
+    }
+
+    void writeTable(SQLite::Database& sqlDb, const core::VirtualTable& table)
+    {
+        createTable(sqlDb, table);
+    }
+
+} // anon namespace
+
+
 
 void core::save_load::SQLiteStrategy::save(const VirtualDatabase &db) const
 {
-    SQLite::Database sqlDb(fSQLiteInfo.filePath, SQLite::OPEN_READWRITE|SQLite::OPEN_CREATE);\
-    dropAllTables(sqlDb);
+    SQLite::Database sqlDb(fSQLiteInfo.filePath, SQLite::OPEN_READWRITE|SQLite::OPEN_CREATE);
     SQLite::Transaction transaction(sqlDb);
+
+    dropAllTables(sqlDb);
+
+    db.forAllTable([&sqlDb] (const core::VirtualTable& table) {
+        writeTable(sqlDb, table);
+    });
+
     transaction.commit();
-
-
 }
 
 std::unique_ptr<core::VirtualDatabase> core::save_load::SQLiteStrategy::load() const
